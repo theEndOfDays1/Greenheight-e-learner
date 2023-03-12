03cc0ce85363336d10b3c4b1509bd88d324870e5
theEndOfDays1 <127692375+theEndOfDays1@users.noreply.github.com>
2023-03-12 22:28:20 +0100

    Add files via upload

---
+++ greenheightscript.js
@@ -0,0 +1,19 @@
+window.addEventListener('scroll', () => {
+    document.querySelector('header').classList.toggle
+    ('window-scroll' , window.scrollY > 0)
+})
+
+
+const openMenu = document.getElementById("menu");
+const closeMenu = document.getElementById("close-menu");
+const header = document.getElementById("header");
+
+openMenu.addEventListener("click", function(){
+    header.style.display = "block"
+})
+closeMenu.addEventListener("click", function(){
+    header.style.display = "none"
+})
+
+/* let recall = window.prompt("Welcome to the Greenheight E-learning community. Please enter your name below");
+document.getElementById("span1").innerHTML = "Hello!"+ "&nbsp" + recall + "." + "<br>" + "This is your personal landing page." + "<br>" + "here, you can access your portal and get up -to-date with the latest information that will be dropped occassionaly."*/
